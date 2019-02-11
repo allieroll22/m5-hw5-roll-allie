cf21451fc91d98f5b611b5a7eb3b9e76252dc130
“Allie <allieroll22@ufl.edu>
2019-02-10 20:27:45 -0500

    Display button on click of boxes

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
 
+//Message on click
 var clickOne = document.getElementById("box1");
 clickOne.onclick = function() {
   alert("Oooh - so close but no cigar")
+  //button on click
+  document.getElementById("button").style.display = "inline";
 }
 
 var clickTwo = document.getElementById("box2");
 clickTwo.onclick = function() {
   alert("DING DING DING - We have a winner")
+  //button on click
+  document.getElementById("button").style.display = "inline";
 }
 
 var clickThree = document.getElementById("box3");
 clickThree.onclick = function() {
   alert("Oops, butter luck next time")
+//button on click
+  document.getElementById("button").style.display = "inline";
 }
 
-
-
-//Background Color Change 
+//Background Color Change
 var bgFunction = document.getElementById("box1");
 var bgFunction1 = document.getElementById("box2");
 var bgFunction2 = document.getElementById("box3");
